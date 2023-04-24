1b3842eecce7991909d49abc3f4045484fcc5e00
liaskar <liaskar@gmail.com>
2023-04-24 13:43:25 +0600

    Скопировал от учителя

--- Cw2_Seminar/Program.cs
+++ Cw2_Seminar/Program.cs
@@ -14,7 +14,39 @@
 
 
 
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine($"{i} OK");
-}
+// for (int i = 0; i < 10; i++)
+// {
+//     Console.WriteLine($"{i} OK");
+// }
+
+
+
+
+// double x = 7.45;
+// double y = 3.875;
+// double result = Math.Pow(x, y);
+
+// Console.WriteLine ($"{result}");
+
+
+
+
+
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+//  int x = new Random().Next(10, 100);
+
+//  int x1 = x / 10;
+//  int x2 = x % 10;
+// Console.WriteLine($"{x}");
+// if (x1 > x2)
+//    Console.WriteLine($"{x1}");
+// else
+//    Console.WriteLine($"{x2}");
+
+
