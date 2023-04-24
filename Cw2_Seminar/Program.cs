1baa10aebb7ff630b08536a70a79aec0678a6422
liaskar <liaskar@gmail.com>
2023-04-24 13:27:17 +0600

    Циклы

---
+++ Cw2_Seminar/Program.cs
@@ -0,0 +1,20 @@
+﻿// // while цикл
+// int x = 5;
+// while (x > 0)
+// {
+//     x--; //x -= 1  x = x - 1
+
+   
+//    if (x == 3 || x == 1)
+//       continue; //breake
+//    Console.WriteLine($"{x} 111");
+// }
+
+// Console.WriteLine($"OK");
+
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{i} OK");
+}
