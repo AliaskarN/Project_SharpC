89a8d0fe13526fde1fcdfd0fe17c7943f3cf8584
liaskar <liaskar@gmail.com>
2023-04-24 07:40:00 +0600

    Add folders

---
+++ Do_1Task/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if(b * b == a)
+{
+    Console.WriteLine("Первое число является квадратом второго");    
+}
---
+++ Project_1stSeminar/Program.cs
@@ -0,0 +1,14 @@
+﻿//int number = 10;
+//int number2 = 15;
+//int number3 = 20;
+//string user = "Bob";
+
+//Console.WriteLine("Hello");
+//Console.WriteLine("Vlad");
+
+//Console.WriteLine($"Hello {user}");
+//Console.WriteLine("Vlad");
+
+Console.Write("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Привет   -   {num + 100}");
