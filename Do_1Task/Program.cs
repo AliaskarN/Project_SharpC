ca5f89e1bb5ca798ea6a048dae2b783b3c006174
liaskar <liaskar@gmail.com>
2023-04-25 09:19:32 +0600

    Save change

--- Do_1Task/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введите первое число");
-int numa = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int numb = Convert.ToInt32(Console.ReadLine());
-
-if(b * b == a)
-{
-    Console.WriteLine("Первое число является квадратом второго");    
-}
